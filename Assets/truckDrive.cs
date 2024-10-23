da5cd12319e45de88fb13150a89e83f337a7e5b9
Aleko Samblanet <aleko.samblanet.2021@gmail.com>
2024-10-23 15:28:53 -0400

    Truck, road, skybox
    need to bake lightmap though

---
+++ Assets/truckDrive.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class truckDrive : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform endTrans;
+    private float wheelRadius = 0.356f;
+    private float wheelCircumference;
+    private float wheelRotPerSec;
+
+    public float speed = 1f;
+    public GameObject[] wheels;
+
+    void Start()
+    {
+        wheelCircumference = 2 * Mathf.PI * wheelRadius;
+        
+    }
+
+    void Update()
+    {
+        //Move Truck
+        float step = Time.deltaTime * speed;
+        transform.position = Vector3.MoveTowards(transform.position, endTrans.position, step);
+
+        wheelRotPerSec = speed / wheelCircumference;
+        foreach (var wheel in wheels)
+        {
+            wheel.transform.Rotate(new Vector3(wheelRotPerSec * -360f * Time.deltaTime, 0, 0));
+        }
+
+    }
+}
